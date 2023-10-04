53f569f9c408af10de286518996c91fec6dca57f
Renzo-codespace <renzo@codespace.co.za>
2023-10-04 21:01:50 +0200

    updates to documentation

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h2>
-              Docs <span>-&gt;</span>
+              Documentation <span>-&gt;</span>
             </h2>
             <p>
               Find in-depth information about Next.js features and&nbsp;API.

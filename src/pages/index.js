fb87cb6714cb3db71a10ef243b4e08630f919186
Renzo-codespace <renzo@codespace.co.za>
2023-10-10 15:15:33 +0200

    Testing Rezno Docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h2>
-              Testing Elzabe Docs <span>-&gt;</span>
+              Testing Rezno Docs <span>-&gt;</span>
             </h2>
             <p>
               Find in-depth information about Next.js features and&nbsp;API.

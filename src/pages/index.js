f65206ffb5d054083077c01751e2b1982089a3a3
Renzo-codespace <renzo@codespace.co.za>
2023-10-10 14:45:55 +0200

    added Testing Delayed Docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h2>
-              Testing Docs <span>-&gt;</span>
+              Testing Delayed Docs <span>-&gt;</span>
             </h2>
             <p>
               Find in-depth information about Next.js features and&nbsp;API.
